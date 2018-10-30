0eae760c6b91222315669757e58674d2a4650601
IFilonov <filonovia@gmail.com>
2018-10-30 22:52:05 +0500

    version and some corrects

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,13 @@ Bundler.require(*Rails.groups)
 module TestGuru
   class Application < Rails::Application
 
-    VERSION = '1.0.0'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.timezone = 'Ekaterinburg'
     #http://rusrails.ru/rails-internationalization-i18n-api
-    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
-    I18n.available_locales = [:ru]
-    I18n.default_locale = :ru
+    config.i18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    config.i18n.available_locales = [:ru]
+    config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
