2e09f758edfa9c05a55940ba472a3a52be54a0bc
IFilonov <filonovia@gmail.com>
2018-11-03 10:29:44 +0500

    models

---
+++ db/migrate/20181103051722_table_not_null.rb
@@ -0,0 +1,10 @@
+class TableNotNull < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :categories :title false
+    change_column_null :tests :title false
+    change_column_null :tests :title false
+    change_column_null :answers :body false
+    change_column_null :questions :body false
+    change_column_null :users :login false
+  end
+end
