17885f62403188e30112860699bae2c029ea63ca
IFilonov <filonovia@gmail.com>
2018-11-03 21:41:20 +0500

    keys,correct,indexes

---
+++ db/migrate/20181103163617_add_index.rb
@@ -0,0 +1,7 @@
+class AddIndex < ActiveRecord::Migration[5.2]
+  def change
+    add_index(:answers, :question_id)
+    add_index(:questions, :test_id)
+    add_index(:tests, :category_id)    
+  end
+end
