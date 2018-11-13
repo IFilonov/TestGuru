1f646e2f334ddde152cad0cee355302a76195d6f
IFilonov <filonovia@gmail.com>
2018-11-13 11:02:31 +0500

    foreign key test author

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :responses
   has_many :tests, through: :responses
+  has_one :test, foreign_key: "author_id"
   def tests_by_level(level)
     Test.joins("join responses on responses.test_id = tests.id")
       .where(level: level, responses: { user_id: id }).pluck(:title)
