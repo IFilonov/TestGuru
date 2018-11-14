95561a75ac1b833f99ff9f8483efa01057d990cb
IFilonov <filonovia@gmail.com>
2018-11-14 22:12:57 +0500

    mothod answer.corrects

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  scope :corrects, -> { where(correct: 'Y') }
+  scope :corrects, -> { where(correct: true) }
 
   validates :body, presence: true
   validate :max_answers, on: :create
