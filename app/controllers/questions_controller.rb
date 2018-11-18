d70c6bf94861213c969fa9d8be958e018e06e276
IFilonov <filonovia@gmail.com>
2018-11-18 23:58:45 +0500

    answer controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,27 @@
+class QuestionsController < ApplicationController
+
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    str_index = "<table>"
+    @test.questions.each { |question| str_index += "<tr> #{question.body} </tr><br>" }
+    str_index += "</table>"
+    render html: str_index.html_safe
+  end
+
+  def show
+   render html: "<i> #{@question.body}</i>".html_safe
+  end
+
+  private
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+end
