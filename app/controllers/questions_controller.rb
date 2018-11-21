34a8693ccd27a3b6c24b4a09637b37fb68c8e809
IFilonov <filonovia@gmail.com>
2018-11-21 14:15:39 +0500

    change create method, +test_guru.html

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,6 @@ class QuestionsController < ApplicationController
 
   before_action :find_test, only: %i[index create]
   before_action :find_question, only: %i[show destroy]
-  skip_before_action :verify_authenticity_token
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
@@ -19,11 +18,16 @@ class QuestionsController < ApplicationController
 
   def create
     question = @test.questions.new(body: params[:body])
-    render html: "<i> #{question.body}</i>".html_safe
+    if question.save
+      render html: "Question <b>#{question.body}</b> created".html_safe
+    else
+      render html: "Question <b>#{question.body}</b> not created".html_safe
+    end
   end
 
   def destroy
-    @question.destroy
+    question = @question.destroy
+    render html: "Question <b>#{question.body}</b> deleted".html_safe
   end
 
   private
@@ -33,5 +37,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render html: "<b>Question not found</b>".html_safe
   end
 end
