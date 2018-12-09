b6e8b22fd202a5ea373d451045d3cc7fdcb266cc
IFilonov <filonovia@gmail.com>
2018-12-09 10:49:50 +0500

    response result view correct

--- app/helpers/responses_helper.rb
+++ app/helpers/responses_helper.rb
@@ -5,4 +5,8 @@ module ResponsesHelper
   def test_result
     @response.correct_questions * 100 / @response.test.questions.count
   end
+
+  def result_success?
+    test_result > SUCCESS_RESULT
+  end
 end
