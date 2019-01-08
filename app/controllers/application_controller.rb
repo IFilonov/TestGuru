c1dbacced6035a209506d272fe01ddbaac377173
IFilonov <filonovia@gmail.com>
2019-01-09 00:51:56 +0500

    views localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
 
+  before_action :set_locale
+
   protected
 
   def after_sign_in_path_for(user)
-    flash.notice = "Привет, #{current_user.first_name}!" if current_user.first_name 
+    flash.notice = "Привет, #{current_user.first_name}!" if current_user.first_name
     current_user.is_a?(Admin) ? admin_tests_path : super
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang])  ? params[:lang] : I18n.default_locale
   end
 end
