2dbe3da99be00957888104bdafadfb5d3bb30d70
IFilonov <filonovia@gmail.com>
2019-01-10 17:13:17 +0500

    default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  def default_url_options
+    locale = I18n.locale == I18n.default_locale ? nil : I18n.locale
+    { lang: locale }
+  end
+
   protected
 
   def after_sign_in_path_for(user)
