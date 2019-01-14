f7301a987a4731284e9b085fcbfa5a0c89345026
IFilonov <filonovia@gmail.com>
2019-01-14 23:37:47 +0500

    gist api with faraday client

---
+++ app/services/gist_question_service.rb
@@ -0,0 +1,36 @@
+class GistQuestionService
+  GIST_TOKEN = ENV['TESTGURU_GIST_TOKEN']
+
+def initialize(question, client: nil)
+    @question = question
+    @test = @question.test
+    @client = client || GitHubClient.new
+  end
+
+  def call
+    @client.create_gist(gist_params)
+  end
+
+  def status_ok?
+    @client.last_response.status == 201
+  end
+
+  private
+
+  def gist_params
+    {
+      description: "A question about #{@test.title} from TestGuru",
+      files: {
+        'test-guru-question.txt' => {
+            content: gist_content
+        }
+      }
+    }
+  end
+
+  def gist_content
+    content = [@question.body]
+    content += @question.answers.pluck(:body)
+    content.join("\n")
+  end
+end
