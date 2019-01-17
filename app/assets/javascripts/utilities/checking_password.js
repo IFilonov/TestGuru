2a7cba480986b208b769ab6dc098736853ece087
IFilonov <filonovia@gmail.com>
2019-01-17 23:28:12 +0500

    add javascript icons on registration form

---
+++ app/assets/javascripts/utilities/checking_password.js
@@ -0,0 +1,26 @@
+document.addEventListener('turbolinks:load', function() {
+  var control = document.querySelector('.check_password_field')
+
+  control.addEventListener('keyup', checkPasswordsEquals)
+})
+
+function checkPasswordsEquals() {
+  var password = document.getElementById('user_password')
+  var password_confirmation = document.getElementById('user_password_confirmation')
+
+  if( password.value.length == 0 || password_confirmation.value.length == 0)
+  {
+    this.querySelector('.octicon-alert').classList.add('hide')
+    this.querySelector('.octicon-thumbsup').classList.add('hide')
+  }
+  else if ( password.value != password_confirmation.value)
+  {
+    this.querySelector('.octicon-alert').classList.remove('hide')
+    this.querySelector('.octicon-alert').classList.add('red')
+    this.querySelector('.octicon-thumbsup').classList.add('hide')
+  }
+  else {
+    this.querySelector('.octicon-alert').classList.add('hide')
+    this.querySelector('.octicon-thumbsup').classList.remove('hide')
+  }
+}
