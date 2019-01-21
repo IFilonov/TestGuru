de13d42720d329830b974fc31e496e5c9eb9652a
IFilonov <filonovia@gmail.com>
2019-01-21 22:34:39 +0500

    Add data attributes on tests form

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,11 +1,11 @@
 class Admin::TestsController < Admin::BaseController
 
-  before_action :find_test, only: %i[show edit update destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy update_inline]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    @tests = Test.all
   end
 
   def show
@@ -40,7 +40,19 @@ class Admin::TestsController < Admin::BaseController
     redirect_to admin_tests_path
   end
 
+  def update_inline
+  if @test.update(test_params)
+    redirect_to admin_tests_path(@test)
+  else
+    render :index
+  end
+end
+
   private
+
+  def set_tests
+    @tests = Test.all
+  end
 
   def find_test
     @test = Test.find(params[:id])
