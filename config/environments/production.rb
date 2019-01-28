7d8b60cdcd96effdfd56cd364ee61bd7d9df52b5
IFilonov <filonovia@gmail.com>
2019-01-28 18:08:14 +0500

    mailer config 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'hidden-gorge-99749.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.gmailcom',
+    address: 'smtp.gmail.com',
     port: 587,
     authentication: 'plain',
     enable_starttls_auto: true,
