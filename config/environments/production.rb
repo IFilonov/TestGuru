2a61f0196bb73cb8bcc70dd04558080552ca6955
IFilonov <filonovia@gmail.com>
2019-01-28 17:16:50 +0500

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "test-guru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'hidden-gorge-99749.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmailcom',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD']
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
